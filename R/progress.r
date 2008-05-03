075093575fcae94d4dd1a9ecc1c64c0204744dfd
hadley <hadley@688fc0e7-72e3-0310-a89e-c7106b9812cc>
2008-05-03 16:43:26 +0000

    Remove old progress bar code

--- R/progress.r
+++ R/progress.r
@@ -1,13 +1,3 @@
-progress_dots <- function() {
-  n <- 0
-  
-  list(
-    init = function(x) n <<- x,
-    step = function()  cat("."),
-    term = function()  cat("\n")
-  )
-}
-
 progress_null <- function() {
   list(
     init = function(x) {},
