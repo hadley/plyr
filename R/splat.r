8c8d838216cd2a0b9d58b0bfc514e5e02c56eb44
hadley <h.wickham@gmail.com>
2008-05-04 09:55:48 -0500

    Add splat docs

--- R/splat.r
+++ R/splat.r
@@ -1,5 +1,8 @@
-#X f <- function(mpg, wt, ...) mpg / wt
-
+# `Splat' arguments to a function
+# Wraps a function in do.call 
+# 
+# @arguments function to splat
+#X f <- function(mpg, wt, ...) data.frame(mw = mpg / wt)
 #X ddply(mtcars, .(cyl), splat(f))
 splat <- function(flat) {
   function(...) do.call(flat, ...)
