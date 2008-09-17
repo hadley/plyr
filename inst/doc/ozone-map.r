615fe4ee83a4248e6c759fd6ba5da1c171236801
hadley <h.wickham@gmail.com>
2008-09-16 21:45:22 -0500

    Writing...

--- inst/doc/ozone-map.r
+++ inst/doc/ozone-map.r
@@ -2,11 +2,11 @@ library(maps)
 outlines <- as.data.frame(map("world",xlim=-c(113.8, 56.2), ylim=c(-21.2, 36.2), plot=FALSE)[c("x","y")])
 map <- c(
   geom_path(aes(x=x, y=y, fill=NULL, group=NULL, order=NULL, size=NULL), data = outlines, colour = alpha("grey20", 0.2)),
-  scale_x_continuous("", limits = c(-113.8, -56.2), breaks=NA),
-  scale_y_continuous("", limits = c(-21.2, 36.2), breaks=NA)
+  scale_x_continuous("", limits = c(-114.8, -55.2), breaks=c(-110, -85, -60)),
+  scale_y_continuous("", limits = c(-22.2, 37.2))
 )
 
-# scale_x_continuous("", limits = c(-113.8, -56.2), breaks=c(-110, -85, -60)),
+# scale_x_continuous("", limits = c(-113.8, -56.2)),
 # scale_y_continuous("", limits = c(-21.2, 36.2))
 
 
@@ -27,10 +27,7 @@ small_mult <- function(df) {
   ggplot(rexpo, aes(x = long + res * rtime, y = lat + res * rozone)) + map  
 }
 
-# small_mult(ozm) + geom_line(aes(group = interaction(lat,long,year)))
-# ggsave(file = "ozone.pdf", width=6, height=6)
-
-oz_stars <- function(data, time, value) {
+make_stars <- function(data, time, value) {
   data[, c(time, value)] <- lapply(data[, c(time, value)], function(x) {
     x <- as.numeric(x)
     (x - min(x)) / diff(range(x))
@@ -43,4 +40,3 @@ oz_stars <- function(data, time, value) {
   })
 }
 # df <- stars(owide[, 1:2], owide[, -(1:2)])
-# ggplot(df, aes(x = long + res * x, y = lat + res * y)) + map + geom_polygon(aes(group = interaction(long, lat)), fill=NA, colour="black")
