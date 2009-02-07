d6c04e190cf71cc699ac5cb621daae0b859e89e0
hadley <h.wickham@gmail.com>
2009-02-07 17:43:15 -0600

    Improve colwise function and documentation

--- R/helper-col-wise.r
+++ R/helper-col-wise.r
@@ -5,12 +5,48 @@
 # on discrete and numeric variables respectively
 # 
 # @arguments function
-# @arguments function that tests columns for inclusion
+# @arguments either function that tests columns for inclusion, or a quoted object giving which columns to process
 # @alias catcolwise
 # @alias numcolwise
-colwise <- function(.fun, .if = function(x) TRUE) {
+#X # Count number of missing values
+#X nmissing <- function(x) sum(is.na(x))
+#X
+#X # Apply to every column in a data frame 
+#X colwise(nmissing)(baseball)
+#X # This syntax looks a little different.  It is shorthand for the 
+#X # the following:
+#X f <- colwise(nmissing)
+#X f(baseball)
+#X
+#X # This is particularly useful in conjunction with d*ply
+#X ddply(baseball, .(year), colwise(nmissing))
+#X 
+#X # To operate only on specified columns, supply them as the second
+#X # argument.  Many different forms are accepted.
+#X ddply(baseball, .(year), colwise(nmissing, .(sb, cs, so)))
+#X ddply(baseball, .(year), colwise(nmissing, c("sb", "cs", "so")))
+#X ddply(baseball, .(year), colwise(nmissing, ~ sb + cs + so))
+#X
+#X # Alternatively, you can specify a boolean function that determines
+#X # whether or not a column should be included
+#X ddply(baseball, .(year), colwise(nmissing, is.character))
+#X ddply(baseball, .(year), colwise(nmissing, is.numeric))
+#X ddply(baseball, .(year), colwise(nmissing, is.discrete))
+#X
+#X # These last two cases are particularly common, so some shortcuts are 
+#X # provided:
+#X ddply(baseball, .(year), numcolwise(nmissing))
+#X ddply(baseball, .(year), catcolwise(nmissing))
+colwise <- function(.fun, .cols = function(x) TRUE) {
+  if (!is.function(.cols)) {
+    .cols <- as.quoted(.cols)
+    filter <- function(df) as.data.frame(eval.quoted(.cols, df))
+  } else {
+    filter <- function(df) Filter(.cols, df)
+  }
+  
   function(df, ...) {
-    filtered <- Filter(.if, df)
+    filtered <- filter(df)
     if (ncol(filtered) == 0) return(data.frame())
     
     df <- do.call("data.frame", alply(filtered, 2, .fun, ...))
@@ -20,9 +56,9 @@ colwise <- function(.fun, .if = function(x) TRUE) {
 }
 
 catcolwise <- function(.fun, .try = FALSE) {
-  colwise(.fun, .if = is.discrete)
+  colwise(.fun, is.discrete)
 }
 numcolwise <- function(.fun, .try = FALSE) {
-  colwise(.fun, .if = is.numeric)
+  colwise(.fun, is.numeric)
 }
 
