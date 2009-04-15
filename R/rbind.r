2b919569412b7285bff5743ceecd89e5722c446a
hadley <h.wickham@gmail.com>
2009-04-15 14:14:30 -0500

    bug fix

--- R/rbind.r
+++ R/rbind.r
@@ -13,6 +13,15 @@
 #X rownames(b1) <- NULL
 #X system.time(b2 <- rbind.fill(bplayer))
 #X stopifnot(all.equal(b1, b2))
+#X 
+#X a <- data.frame(a = factor(letters[1:3]), b = 1:3, c = date())
+#X b <- data.frame(a = factor(letters[3:5]), 
+#X    d = as.Date(c("2008-01-01", "2009-01-01", "2010-01-01")))
+#X ab1 <- rbind.fill(a, b)[, letters[1:4]]
+#X ab2 <- rbind.fill(b, a)[c(4:6, 1:3), letters[1:4]]
+#X ab2$a <- factor(ab2$a, levels(ab1$a))
+#X rownames(ab2) <- NULL
+#X stopifnot(all.equal(ab1, ab2))
 rbind.fill <- function(...) {
   dfs <- list(...)
   if (length(dfs) == 0) return(list())
@@ -21,14 +30,18 @@ rbind.fill <- function(...) {
   }
   
   rows <- unlist(lapply(dfs, nrow))
+  nrows <- sum(rows)
   
   output <- list()
+  seen <- character()
 
   # Set up factors
   factors <- names(dfs[[1]])[laply(dfs[[1]], is.factor)]
   for(var in factors) {
     all <- llply(dfs, function(df) levels(df[[var]]))
     output[[var]] <- factor(levels = unique(unlist(all)))
+    length(output[[var]]) <- nrows
+    seen <- c(seen, var)
   }
 
   # Compute start and end positions for each matrix
@@ -40,16 +53,19 @@ rbind.fill <- function(...) {
     df <- dfs[[i]]
     
     for(var in names(df)) {
-      if (length(df[[var]]) > 0) output[[var]][rng] <- df[[var]]
+      if (length(df[[var]]) > 0) {
+        output[[var]][rng] <- df[[var]]
+        
+        if (!var %in% seen) {
+          length(output[[var]]) <- nrows
+          attributes(output[[var]]) <- attributes(df[[var]])
+          
+          seen <- c(seen, var)
+        }
+      }
     }
   }
 
-  # Ensure all variables are the same length.  They might not be if the 
-  # last data frame does not contain all rows.
-  for(var in names(output)) {
-    length(output[[var]]) <- sum(rows)
-  }
-  
   as_df(output)
 }
 
