23ab2602908ea8c9521ee9e7e044563451adc096
hadley <h.wickham@gmail.com>
2009-06-23 07:43:06 -0500

    Move summarise helper to correct place and add documentation

---
+++ R/helper-summarise.r
@@ -0,0 +1,21 @@
+# Summarise a data frame.
+# Create a new data frame that summarises an existing data frame.
+# 
+# Summarise works in an analagous way to transform, except instead of adding
+# columns to an existing data frame, it creates a new one.  This is
+# particularly useful in conjunction with \code{\link{ddply}} as it makes it
+# easy to perform group-wise summaries.
+# 
+# @arguments The data frame to be summarised
+# @arguments Further arguments of the form var = value
+#X summarise(baseball, 
+#X  duration = max(year) - min(year), 
+#X  nteams = length(unique(team)))
+#X ddply(baseball, "id", summarise, 
+#X  duration = max(year) - min(year), 
+#X  nteams = length(unique(team)))
+summarize <- summarise <- function(`_data`, ...) {
+ eval(substitute(data.frame(...)), `_data`, parent.frame())
+}
+
+# Alternative names: tally?
--- helper-summarise.r
+++
@@ -1,3 +0,0 @@
-summarize <- summarise <- function(.data, ...) {
- eval(substitute(data.frame(...)), .data, parent.frame())
-}
