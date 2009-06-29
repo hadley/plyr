e99638ae610b27c4f5d007a6d1b9ccd2f1d12d1f
hadley <h.wickham@gmail.com>
2009-06-29 08:40:21 -0500

    Add iterator that adds hasNext() behaviour. Plus a few convenience methods that the iterators package should probably provide.

---
+++ R/iterator.r
@@ -0,0 +1,45 @@
+library(iterators)
+icanhasnext <- function(iterator) {
+  # If already has hasNext function return iterator unchanged
+  if (!is.null(iterator$hasNext)) return(iterator)
+  
+  cache <- NULL
+  has_next <- NA
+  
+  nextElem <- function() {
+    if (!hasNext()) end_iteration()
+    
+    # Return value and reset cache
+    has_next <<- NA
+    cache
+  }
+  
+  hasNext <- function() {
+    if (!is.na(has_next)) return(has_next)
+    
+    tryCatch({
+      cache <<- iterator$nextElem()
+      has_next <<- TRUE
+    },
+    error = function(e) {
+      if (iteration_has_ended(e)) {
+        has_next <<- FALSE
+      } else {
+        stop(e)
+      }
+    })
+    
+    has_next
+  }
+  
+  new_iterator(nextElem, hasNext = hasNext)
+}
+end_iteration <- function() stop('StopIteration', call.=FALSE)
+iteration_has_ended <- function(e) {
+  identical(conditionMessage(e), 'StopIteration')
+}
+new_iterator <- function(nextElem, ...) {
+  structure(list(nextElem=nextElem, ...), class=c('abstractiter', 'iter'))
+}
+
+is.iterator <- function(x) inherits(x, "iterator")
