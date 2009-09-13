eaa2c98ba461e6481c0e214bb049e7cfc7fc86e0
hadley <h.wickham@gmail.com>
2009-09-13 14:15:34 -0500

    First attempt at merge replacement

---
+++ R/join.r
@@ -0,0 +1,78 @@
+# bsmall <- subset(baseball, id %in% sample(unique(baseball$id), 10))[, 1:5]
+# bsmall$id <- factor(bsmall$id)
+# bsmall <- bsmall[with(bsmall, order(id, year, stint)), ]
+# bsmall <- bsmall[sample(rownames(bsmall)),  ]
+# rownames(bsmall) <- NULL
+
+# first <- ddply(baseball, "id", summarise, first = min(year))
+# system.time(b2 <- merge(baseball, first, by = "id", all.x = T))
+# system.time(b3 <- join(baseball, first, by = "id"))
+# 
+# b2 <- arrange(b2, id, year, stint)
+# b3 <- arrange(b3, id, year, stint)
+# rownames(b2) <- NULL
+# rownames(b3) <- NULL
+# all.equal(b2, b3)
+
+# 
+# geo1 <- merge(sales, ad, by = c("street", "city", "zip"), all.x = TRUE)
+# geo1 <- arrange(geo1, zip, city, street, date)
+# rownames(geo1) <- NULL
+# 
+# geo2 <- join(sales, ad, by = c("street", "city", "zip"))
+# geo2 <- arrange(geo2, zip, city, street, date)
+# rownames(geo2) <- NULL
+# 
+# system.time(join(sales, ad, by = c("street", "city", "zip")))
+# 
+# all.equal(geo1, geo2)
+
+
+# Join
+#
+# Unlike merge, preserves the order of x no matter what join type is used.
+# If needed, rows from y will be added to the bottom.
+join <- function(x, y, by = intersect(names(x), names(y)), type = "left") {
+  type <- match.arg(type, c("left", "right", "inner", "full"))
+  
+  if (missing(by)) {
+    message("Joining by: ", paste(by, collapse = ", "))
+  }
+  
+  keys <- join.keys(x, y, by = by)
+  new.cols <- setdiff(names(y), by)
+  
+  if (type == "inner") {
+    x.match <- match(keys$y, keys$x, 0)
+    y.match <- match(keys$x, keys$y, 0)
+    cbind(x[x.match, , drop = FALSE], y[y.match, new.cols, drop = FALSE])
+  } else if (type == "left") {
+    y.match <- match(keys$x, keys$y)
+    cbind(x, y[y.match, new.cols, drop = FALSE])
+  } else if (type == "right") {
+    if (any(duplicated(keys$y))) {
+      stop("Duplicated key in y", call. = FALSE)
+    }
+    
+    x.match <- match(keys$y, keys$x)
+    cbind(x[x.match, , drop = FALSE], y[, new.cols, drop = FALSE])
+  } else if (type == "full") {
+    stop("Not implemented yet")
+  }
+}
+
+join.keys <- function(x, y, by) {
+  joint <- rbind.fill(x[by], y[by])
+  
+  factors <- sapply(joint, is.factor)
+  joint[!factors] <- lapply(joint[!factors], function(x) {
+    factor(x, levels = unique(x))
+  })
+  
+  keys <- ninteraction(joint)
+  
+  list(
+    x = keys[1:nrow(x)],
+    y = keys[-(1:nrow(x))]
+  )
+}
---
+++ tests/test-join.r
@@ -0,0 +1,41 @@
+context("Join")
+
+a <- data.frame(a = 1:10, b = letters[1:10])
+a_even <- a[seq(2, 10, by = 2), ]
+a_odd <- a[seq(1, 9, by = 2), ]
+b <- data.frame(a = 1:10, c = LETTERS[1:10])
+b_even <- b[seq(2, 10, by = 2), ]
+b_odd <- b[seq(1, 9, by = 2), ]
+
+# Create smaller subset of baseball data (for speed)
+bsmall <- subset(baseball, id %in% sample(unique(baseball$id), 20))[, 1:5]
+bsmall$id <- factor(bsmall$id)
+bsmall <- bsmall[sample(rownames(bsmall)),  ]
+rownames(bsmall) <- NULL
+
+first <- ddply(bsmall, "id", summarise, first = min(year))
+
+test_that("results consistent with merge", {  
+  b2 <- merge(bsmall, first, by = "id", all.x = T)
+  b3 <- join(bsmall, first, by = "id")
+  
+  b2 <- arrange(b2, id, year, stint)
+  b3 <- arrange(b3, id, year, stint)
+  rownames(b2) <- NULL
+  rownames(b3) <- NULL
+  
+  expect_that(b2, equals(b3))
+})
+
+test_that("order is preserved", {
+  b3 <- join(bsmall, first, by = "id")
+
+  expect_that(bsmall$id, equals(b3$id))
+  expect_that(bsmall$year, equals(b3$year))
+  expect_that(bsmall$stint, equals(b3$stint))  
+})
+
+test_that("duplicated keys are an error", {
+  
+  
+})
