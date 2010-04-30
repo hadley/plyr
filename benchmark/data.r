8e5fa22003807494214c2d5d893404adfb93ebca
hadley <h.wickham@gmail.com>
2010-04-30 14:40:50 -0500

    Construct both character and factor variables

--- benchmark/data.r
+++ benchmark/data.r
@@ -1,10 +1,12 @@
 # Generate sample data sets for benchmarking
+options(stringsAsFactors = FALSE)
 
 make_data <- function(n) {
   data.frame(
     unif = runif(n),
     norm = rnorm(n),
-    letter = sample(letters, n, rep = T),
+    char = sample(letters, n, rep = T),
+    factor = factor(sample(letters, n, rep = T)),
     group_a = rep(1:100, length = n),
     group_b = rep(1:1000, length = n),
     group_c = rep(1:10000, length = n)
