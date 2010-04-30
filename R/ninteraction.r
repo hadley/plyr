33d039c7def7e1ea18f6174004fc51f7d68a3bc1
hadley <h.wickham@gmail.com>
2010-04-30 14:29:06 -0500

    Refactor ninteraction and rename to id

--- R/ninteraction.r
+++ R/ninteraction.r
@@ -1,52 +1,68 @@
-#' Numerical interaction
-#' A purely numerical interaction function that powers \code{aaply}.
-#' 
+#' A unique numeric id for each unique row in a data frame.
+#'
+#' Properties:
+#' \itemize{
+#'   \item \code{order(id)} is equivalent to \code{do.call(order, df)}
+#'   \item rows containing the same data have the same value
+#'   \item if \code{drop = FALSE} then room for all possibilites
+#' }
+#'
 #' @param .variables list of variables
 #' @param drop drop unusued factor levels?
+#' @return a numeric vector with attribute n, giving total number of
+#'   possibilities
 #' @keywords internal
-ninteraction <- function(.variables, drop = FALSE) {  
+#' @seealso \code{\link{id_var}}
+#' @alias id ninteraction
+id <- function(.variables, drop = FALSE) {
   if (length(.variables) == 0) {
     res <- structure(rep.int(1L, nrow(.variables)), n = 1L)
     return(res)
   }
-  
+
   # Special case for single variable
   if (length(.variables) == 1) {
-    var <- .variables[[1]]
-    
-    f <- addNA(as.factor(var), ifany = TRUE)
-    res <- structure(as.integer(f), n = nunique(f))
-    return(res)
+    id_var(.variables[[1]], drop = drop)
   }
-  
-  # Convert to factors, if necessary
-  not_factor <- !unlist(lapply(.variables, is.factor))
-  .variables[not_factor] <- lapply(.variables[not_factor], factor,
-    exclude=NULL)
-  .variables <- lapply(.variables, addNA, ifany = TRUE)
-  
-  # Calculate dimensions
 
-  ndistinct <- unlist(lapply(.variables, nunique))
+  # Calculate individual ids
+  ids <- lapply(.variables, id_var)
+
+  # Calculate dimensions
+  ndistinct <- unlist(lapply(ids, attr, "n"))
   n <- prod(ndistinct)
 
-  p <- length(.variables)
+  p <- length(ids)
   combs <- c(1, cumprod(ndistinct[-p]))
-  
-  mat <- do.call("cbind", lapply(.variables, as.integer))
+
+  mat <- do.call("cbind", ids)
   res <- c((mat - 1L) %*% combs + 1L)
-  
+  attr(res, "n") <- n
+
   # vdf <- data.frame(.variables)
   # names(vdf) <- paste("X", 1:ncol(vdf), sep="")
   # vdf$i <- res
   # browser()
-  
+
   if (drop) {
-    f <- factor(res) 
-    n <- nunique(f)
-    res <- as.integer(f)
+    res <- id_var(res)
   }
-  
-  attr(res, "n") <- n
+
   res
+}
+ninteraction <- id
+
+#' Numeric id for a vector
+#' @keywords internal
+id_var <- function(x, drop = TRUE) {
+  if (drop && is.factor(x)) x <- factor(x)
+  id <- as.numeric(addNA(as.factor(x), ifany = TRUE))
+
+  if (is.factor(x)) {
+    n <- length(levels(x))
+  } else {
+    n <- max(x)
+  }
+
+  structure(id, n = n)
 }
---
+++ inst/tests/test-ninteraction.r
@@ -0,0 +1,53 @@
+context("ninteraction")
+
+simple_vectors <- list(
+  letters,
+  sample(letters),
+  1:26
+)
+
+test_that("for vector, equivalent to rank", {
+  for(case in simple_vectors) {
+    rank <- rank(case, ties = "min")
+    rank_df <- ninteraction(as.data.frame(case))
+
+    expect_that(rank, is_equivalent_to(rank_df))
+  }
+})
+
+test_that("duplicates numbered sequentially", {
+  for(case in simple_vectors) {
+    rank <- rep(rank(case, ties = "min"), each = 2)
+    rank_df <- ninteraction(as.data.frame(rep(case, each = 2)))
+
+    expect_that(rank, is_equivalent_to(rank_df))
+  }
+})
+
+test_that("n calculated correctly", {
+  n <- function(x) attr(ninteraction(x), "n")
+  for(case in simple_vectors) {
+    expect_that(n(as.data.frame(case)), equals(26))
+  }
+
+})
+
+test_that("for vector + constant, equivalent to rank", {
+  for(case in simple_vectors) {
+    rank <- rank(case, ties = "min")
+
+    after <- ninteraction(data.frame(case, x = 1))
+    before <- ninteraction(data.frame(x = 1, case))
+
+    expect_that(rank, is_equivalent_to(before))
+    expect_that(rank, is_equivalent_to(after))
+  }
+})
+
+test_that("grids are correctly ranked", {
+  df <- expand.grid(1:10, 1:2)
+
+  expect_that(ninteraction(df), is_equivalent_to(1:20))
+  expect_that(ninteraction(df, drop = T), is_equivalent_to(1:20))
+})
+
