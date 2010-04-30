95fcc9ebbad37dffd3fb6b1e46ef682153230952
hadley <h.wickham@gmail.com>
2010-04-30 15:14:00 -0500

    Two variable timings

--- benchmark/bench-id.r
+++ benchmark/bench-id.r
@@ -5,3 +5,6 @@ if (!exists("bench6")) load("benchmark.rdata")
 bench("num", id(bench6["group_a"]))
 bench("chr", id(bench6["char"]))
 bench("fac", id(bench6["factor"]))
+
+bench("num-num", id(bench6[c("group_a", "group_b")]))
+bench("num-fac", id(bench6[c("group_a","factor")]))
