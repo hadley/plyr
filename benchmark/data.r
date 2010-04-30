5aabcbae855376fe1f8462abf39d96a7d2143edd
hadley <h.wickham@gmail.com>
2010-04-30 14:37:39 -0500

    Benchmarking data

---
+++ benchmark/data.r
@@ -0,0 +1,18 @@
+# Generate sample data sets for benchmarking
+
+make_data <- function(n) {
+  data.frame(
+    unif = runif(n),
+    norm = rnorm(n),
+    letter = sample(letters, n, rep = T),
+    group_a = rep(1:100, length = n),
+    group_b = rep(1:1000, length = n),
+    group_c = rep(1:10000, length = n)
+  )
+}
+
+bench4 <- make_data(10 ^ 4)
+bench5 <- make_data(10 ^ 5)
+bench6 <- make_data(10 ^ 6)
+
+save(bench4, bench5, bench6, file = "benchmark.rdata")
