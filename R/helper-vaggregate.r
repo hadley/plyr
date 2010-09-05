304b2135bacba71b89caf5144f5d0a21040e7273
hadley <h.wickham@gmail.com>
2010-09-05 13:23:47 -0500

    More about vaggregate

--- R/helper-vaggregate.r
+++ R/helper-vaggregate.r
@@ -1,10 +1,14 @@
 #' Vector aggregate.
 #'
+#' This function is somewhat similar to \code{tapply}, but is designed for
+#' use in conjunction with \code{id}.
+#' 
 #' @param .value vector of values to aggregate
 #' @param .group grouping vector
 #' @param .fun aggregation function
 #' @param ... other arguments passed on to \code{.fun}
-#' @param .default default value used for missing groups
+#' @param .default default value used for missing groups.  This argument is 
+#'   also used as the template for function output.
 #' @param .n total number of groups
 #' @export
 vaggregate <- function(.value, .group, .fun, ..., .default = NULL, .n = max(group)) {
