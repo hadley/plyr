7bd2b7ff25643a3cacebbee7bb29baa1b9e4efd2
hadley <h.wickham@gmail.com>
2010-09-08 16:52:53 -0500

    Move round_any from reshape

---
+++ R/helper-round-any.r
@@ -0,0 +1,21 @@
+#' Round to multiple of any number
+#' 
+#' @param x numeric vector to round
+#' @param accuracy number to round to
+#' @param f rounding function: \code{\link{floor}}, \code{\link{ceiling}} or 
+#'  \code{\link{round}}
+#' @keywords manip
+#' @export
+#' @examples
+#' round_any(135, 10)
+#' round_any(135, 100)
+#' round_any(135, 25)
+#' round_any(135, 10, floor)
+#' round_any(135, 100, floor)
+#' round_any(135, 25, floor)
+#' round_any(135, 10, ceiling)
+#' round_any(135, 100, ceiling)
+#' round_any(135, 25, ceiling)
+round_any <- function(x, accuracy, f=round) {
+  f(x / accuracy) * accuracy
+}
