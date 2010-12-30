542ac8ec28a80d2cd3fcb80fbe03ca02942d3ce4
hadley <h.wickham@gmail.com>
2010-12-30 12:58:42 -0600

    Weighted counts

--- R/helper-count.r
+++ R/helper-count.r
@@ -2,36 +2,64 @@
 #' Equivalent to \code{as.data.frame(table(x))}, but does not include
 #' combinations with zero counts.
 #'
-#' Count is speed competitive with \code{\link{table}} for single variables,
-#' but it really comes into its own when summarising multiple dimensions that
-#' are not crossed. 
+#' Speed-wise count is competitive with \code{\link{table}} for single
+#' variables, but it really comes into its own when summarising multiple
+#' dimensions because it only counts combinations that actually occur in the
+#' data.
+#'
+#' Compared to \code{\link{table}} + \code{\link{as.data.frame}}, \code{count}
+#' also preserves the type of the identifier variables, instead of converting
+#' them to characters/factors.
 #'
 #' @param df data frame to be processed
 #' @param vars variables to count unique values of
+#' @param wt_var optional variable to weight by - if this is non-NULL, count
+#'   will sum up the value of this variable for each combination of id
+#'   variables.
 #' @return a data frame with label and freq columns
 #' @keywords manip
 #' @export
 #' @examples
 #' count(baseball, "id")
+#' count(baseball, "id", "g")
+#' count(baseball, "id", "ab")
 #' count(baseball, "lg")
 #' count(baseball, "stint")
+#' count(count(baseball, c("id", "year")), "id", "freq")
 #' count(count(baseball, c("id", "year")), "freq")
-count <- function(df, vars = NULL) {
+count <- function(df, vars = NULL, wt_var = NULL) {
   if (is.vector(df)) {
     df <- data.frame(x = df)
   }
   
   if (!is.null(vars)) {
     vars <- as.quoted(vars)
-    df <- quickdf(eval.quoted(vars, df))
+    df2 <- quickdf(eval.quoted(vars, df))
+  } else {
+    df2 <- df
   }
-
-  id <- ninteraction(df, drop = TRUE)
-
+  
+  id <- ninteraction(df2, drop = TRUE)
   u_id <- !duplicated(id)
-  labels <- df[u_id, , drop = FALSE]
+  labels <- df2[u_id, , drop = FALSE]
   labels <- labels[order(id[u_id]), , drop = FALSE]
-  freq <- tabulate(id, attr(id, "n"))
+  
+  if (is.null(wt_var) && "freq" %in% names(df)) {
+    message("Using freq as weighting variable")
+    wt_var <- "freq"
+  }
+  
+  if (!is.null(wt_var)) {
+    wt_var <- as.quoted(wt_var)
+    if (length(wt_var) > 1) {
+      stop("wt_var must be a single variable", call. = FALSE)
+    }
+    
+    wt <- eval.quoted(wt_var, df)[[1]]
+    freq <- vaggregate(wt, id, sum, .default = 0)
+  } else {
+    freq <- tabulate(id, attr(id, "n"))    
+  }
   
   unrowname(data.frame(labels, freq))
 }
--- inst/tests/test-count.r
+++ inst/tests/test-count.r
@@ -28,3 +28,22 @@ test_that("random order doesn't affect count", {
   }
   
 })
+
+test_that("weighted count matches xtab", {
+  xt1 <- as.data.frame(xtabs(g ~ id, data = baseball), responseName = "freq")
+  xt1$id <- as.character(xt1$id)
+  ct1 <- count(baseball, "id", "g")
+  
+  expect_that(ct1, equals(xt1))
+  
+  xt2 <- as.data.frame(xtabs(g ~ year + team, data = baseball), 
+    responseName = "freq")
+  xt2 <- subset(xt2, freq > 0)
+  xt2$year <- as.numeric(as.character(xt2$year))
+  xt2$team <- as.character(xt2$team)
+  xt2 <- arrange(xt2, year, team)
+  ct2 <- count(baseball, c("year", "team"), "g")
+  
+  expect_that(ct2, equals(xt2))
+  
+})
