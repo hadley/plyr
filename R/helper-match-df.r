dcce34adf050efc5931e6699f2c09aa14dab7408
hadley <h.wickham@gmail.com>
2011-01-15 18:46:35 -0600

    New match_df function

---
+++ R/helper-match-df.r
@@ -0,0 +1,25 @@
+#' Extract matching rows of a data frame.
+#'
+#' This is particularly useful when you've summarised the data in some way
+#' and want to subset the original data by a characteristic of the subset.
+#'
+#' @param x data frame to subset.
+#' @param match data frame defining matching rows.
+#' @param on variables to match on - by default all variables common to both
+#'   data frames will be used.
+#' @return a data frame
+#' @seealso \code{\link{join}} to combine the columns from both x and y
+#' @export
+#' @examples
+#' longterm <- subset(count(baseball, "id"), freq > 25)
+#' bb_longterm <- match_df(baseball, longterm)
+match_df <- function(x, y, on = NULL) {
+  if (is.null(on)) {
+    on <- intersect(names(x), names(y))
+    message("Matching on: ", str_c(on, collapse = ", "))
+  }
+  
+  keys <- join.keys(x, y, on)
+  x[match(keys$x, keys$y, nomatch = 0), ]
+}
+
