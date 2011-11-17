66163313574c3c0819cb5cd48665d14e962fca0d
Scott-Kostyshak <skostysh@princeton.edu>
2011-11-17 00:46:55 -0500

    add test that ldply handles NA column names when returned by .fun

--- inst/tests/test-list.r
+++ inst/tests/test-list.r
@@ -19,3 +19,10 @@ test_that("list names are preserved", {
   expect_that(names(llply(a)), equals(letters[1:10]))
   
 })
+
+test_that("ldply handles NA column names when returned by .fun", {
+ res <- ddply(data.frame(health = "very good", age=3), .(age), function(x) table(x$health,useNA = "always"))
+ expected_res <- structure(list(age = 3, `very good` = 1L, `NA` = 0L), .Names = c("age", "very good", NA), row.names = c(NA, -1L), class = "data.frame")
+
+ expect_that(res, is_identical_to(expected_res))
+})
