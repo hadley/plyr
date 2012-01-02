99775292f6a7591bfde935869e660d7b7767e4af
hadley <h.wickham@gmail.com>
2012-01-02 11:10:35 -0600

    Take function to subset along arbitrary dimension

---
+++ R/helper-take.r
@@ -0,0 +1,25 @@
+#' Take a subset along an arbitrary dimension
+#' 
+#' @param x matrix or array to subset
+#' @param along dimension to subset along
+#' @param indices 
+#' @param drop should the dimensions of the array be simplified? Defaults
+#'   to \code{FALSE} which is the opposite of the useful R default.
+#' @export
+#' @examples
+#' x <- array(seq_len(3 * 4 * 5), c(3, 4, 5))
+#' take(x, 3, 1)
+#' take(x, 2, 1)
+#' take(x, 1, 1)
+#' take(x, 3, 1, drop = TRU)
+#' take(x, 2, 1, drop = TRU)
+#' take(x, 1, 1, drop = TRU)
+take <- function(x, along, indices, drop = FALSE) {
+  nd <- length(dim(x))
+  
+  index <- as.list(rep(TRUE, nd))
+  index[along] <- indices
+  
+  eval(as.call(c(as.name("["), as.name("x"), index, drop = drop)))
+}
+
