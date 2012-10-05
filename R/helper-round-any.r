d181cba7b79544b355d2e88b0c8c07055448cf15
hadley <h.wickham@gmail.com>
2012-10-05 16:43:28 -0500

    Update news and docs

--- R/helper-round-any.r
+++ R/helper-round-any.r
@@ -1,8 +1,8 @@
 #' Round to multiple of any number.
-#' 
+#'
 #' @param x numeric or date-time (POSIXct) vector to round
 #' @param accuracy number to round to; for POSIXct objects, a number of seconds
-#' @param f rounding function: \code{\link{floor}}, \code{\link{ceiling}} or 
+#' @param f rounding function: \code{\link{floor}}, \code{\link{ceiling}} or
 #'  \code{\link{round}}
 #' @keywords manip
 #' @export
@@ -17,8 +17,8 @@
 #' round_any(135, 100, ceiling)
 #' round_any(135, 25, ceiling)
 #'
-#' round_any(Sys.time()+1:10, 5)
-#' round_any(Sys.time()+1:10, 5, floor)
+#' round_any(Sys.time() + 1:10, 5)
+#' round_any(Sys.time() + 1:10, 5, floor)
 #' round_any(Sys.time(), 3600)
 round_any <- function(x, accuracy, f = round) {
   UseMethod("round_any")
@@ -29,10 +29,7 @@ round_any.numeric <- function(x, accuracy, f = round) {
 }
 
 round_any.POSIXct <- function(x, accuracy, f = round) {
-  # extract time zone
   tz <- format(x[1], "%Z")
-  # round to number of seconds
-  xr <- round_any.numeric(as.numeric(x), accuracy, f)
-  # reconvert into a time
-  return(as.POSIXct(xr, origin="1970-01-01 00:00.00 UTC", tz=tz))
+  xr <- round_any(as.numeric(x), accuracy, f)
+  as.POSIXct(xr, origin="1970-01-01 00:00.00 UTC", tz=tz)
 }
