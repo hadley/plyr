7a96653d3576aedfe2bd206b34cedc41355dfb30
hadley <h.wickham@gmail.com>
2012-10-08 11:21:49 -0500

    Better matchdf docs. Fixes #42

--- R/helper-match-df.r
+++ R/helper-match-df.r
@@ -1,7 +1,18 @@
 #' Extract matching rows of a data frame.
 #'
-#' This is particularly useful when you've summarised the data in some way
+#' Match works in the same way as join, but instead of return the combined
+#' dataset, it only returns the matching rows from the first dataset. This is
+#' particularly useful when you've summarised the data in some way
 #' and want to subset the original data by a characteristic of the subset.
+#'
+#' \code{match_df} shares the same semantics as \code{\link{join}}, not
+#' \code{\link{match}}:
+#'
+#' \itemize{
+#'   \item the match criterion is \code{==}, not \code{\link{identical}}).
+#'   \item it doesn't work for columns that are not atomic vectors
+#'   \item if there are no matches, the row will be omitted'
+#' }
 #'
 #' @param x data frame to subset.
 #' @param y data frame defining matching rows.
@@ -30,5 +41,5 @@ match_df <- function(x, y, on = NULL) {
   }
 
   keys <- join.keys(x, y, on)
-  x[keys$x %in% keys$y, ]
+  x[keys$x %in% keys$y, , drop = FALSE]
 }
