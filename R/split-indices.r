9ddb44429736b551af1c5279fd5a5ed0ec582f14
hadley <h.wickham@gmail.com>
2012-10-28 09:09:25 -0500

    Explore split_indices

--- R/split-indices.r
+++ R/split-indices.r
@@ -8,6 +8,7 @@
 #' @param n largest integer (may not appear in index)
 #' @useDynLib plyr
 #' @keywords internal manip
+#' @export
 split_indices <- function(group, n = max(group)) {
   if (length(group) == 0) return(integer())
   stopifnot(is.integer(group))
