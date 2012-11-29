d03400b60e49f6086122ab56acf53a4374a33c1b
Winston Chang <winston@stdout.org>
2012-11-29 17:37:46 -0600

    Revert C code changes from 03e1af8 because they don't fix the problem

--- src/split-numeric.c
+++ src/split-numeric.c
@@ -10,34 +10,31 @@ SEXP split_indices(SEXP group, SEXP n) {
   int *pgroup = INTEGER(group);
   
   // Count number of cases in each group
-  SEXP counts;
-  PROTECT(counts = allocVector(INTSXP, nlevs));
-  int *pcounts = INTEGER(counts);
-  
+  int counts[nlevs];
   for (i = 0; i < nlevs; i++)
-    pcounts[i] = 0;
+    counts[i] = 0;
   for (i = 0; i < nobs; i++) {
     j = pgroup[i];
     if (j > nlevs) error("n smaller than largest index");
-    pcounts[j - 1]++;
+    counts[j - 1]++;
   }
 
   // Allocate storage for results
   PROTECT(vec = allocVector(VECSXP, nlevs));
   for (i = 0; i < nlevs; i++) {
-    SET_VECTOR_ELT(vec, i, allocVector(INTSXP, pcounts[i]));
+    SET_VECTOR_ELT(vec, i, allocVector(INTSXP, counts[i]));
   }
 
   // Put indices in groups
   for (i = 0; i < nlevs; i++) {
-    pcounts[i] = 0;
+    counts[i] = 0;
   }
   for (i = 0; i < nobs; i++) {
     j = pgroup[i] - 1;
-    k = pcounts[j];
+    k = counts[j];
     INTEGER(VECTOR_ELT(vec, j))[k] = i + 1;
-    pcounts[j]++;
+    counts[j]++;
   }
-  UNPROTECT(2);
+  UNPROTECT(1);
   return vec;
 }
