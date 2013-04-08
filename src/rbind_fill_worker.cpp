4d7b34f9ebd56ead315644e4621bd689b398b23e
muelleki <kirill.mueller@ivt.baug.ethz.ch>
2013-04-08 16:20:12 +0200

    rename Rcpp version to rbind.fill.worker.rcpp (transitional)

--- src/rbind_fill_worker.cpp
+++ src/rbind_fill_worker.cpp
@@ -2,7 +2,7 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// [[Rcpp::export(rbind.fill.worker)]]
+// [[Rcpp::export(rbind.fill.worker.rcpp)]]
 List rbind_fill_worker(List output, DataFrame dfs,
                        IntegerVector rows, DataFrame pos)
 {
