65697519b00d63d2cfaf5b67a20602a6b61805dc
hadley <h.wickham@gmail.com>
2013-11-27 09:42:02 -0600

    Convert to Rcpp

--- inst/tests/test-split-indices.r
+++ inst/tests/test-split-indices.r
@@ -2,7 +2,10 @@ context("Split indices")
 
 test_that("Error if n too small", {
   expect_error(split_indices(1:10, -5), "n must be")
-  expect_error(split_indices(1:10, 5), "n smaller than largest index")
+})
+
+test_that("index expands if n too small", {
+  expect_equal(split_indices(1:10, 5), as.list(1:10))
 })
 
 test_that("succeeds for large number of groups", {
