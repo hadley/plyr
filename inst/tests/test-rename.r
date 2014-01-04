24cda0056278a14db6c688c61123c6e97bcb13d2
wibeasley <wibeasley@hotmail.com>
2014-01-03 21:02:23 -0600

    Gives error/warning/message/silent if renames are duplicated.
    The test suite sometimes gives errors like "rbind.fill performance linear with factors -
    p[2]/p[1] < threshold isn't true".  But I think it's unrelated to changes I've made to `plyr::rename()`.

--- inst/tests/test-rename.r
+++ inst/tests/test-rename.r
@@ -1,9 +1,12 @@
-context("Rename")
+#################################################
+### Main-stream cases
+#################################################
+context("Rename - Expected Usage")
 
 test_that("No match leaves names unchanged", {
   x <- c(a = 1, b = 2, c = 3, 4)
   y <- rename(x, c(d = "e"), warn_missing = FALSE)
-
+  
   expect_equal(names(x), names(y))
 })
 
@@ -16,14 +19,14 @@ test_that("Missing old values result in message", {
 test_that("Single name match makes change", {
   x <- c(a = 1, b = 2)
   y <- rename(x, c(b = "c"))
-
+  
   expect_equal(names(y), c("a", "c"))
 })
 
 test_that("Multiple names correctly changed", {
   x <- c(a = 1, b = 2, c = 3)
   y <- rename(x, c("c" = "f", "b" = "e", "a" = "d"))
-
+  
   expect_equal(names(y), c("d", "e", "f"))
 })
 
@@ -38,14 +41,20 @@ test_that("Renaming lists", {
   expect_identical(y, list(d = 1, e = 2, f = 3))
 })
 
+#################################################
+### Duplicate Names
+#################################################
 context("Rename - Duplicates")
 
+##
+## This batch tests the typical renaming scenarios
+##
 test_that("Renaming list with an conflicting variable name - default", {
   x <- list(a = 1, b = 2, c = 3)
   replace_list <- c("c" = "f", "b" = "e", "a" = "f")
   expect_warning(object = rename(x=x, replace=replace_list))
 })
-test_that("Renaming list with an conflicting variable name - warning", {
+test_that("Renaming list with an conflicting variable name - error", {
   duplicate_behavior <- "error"
   x <- list(a = 1, b = 2, c = 3)
   replace_list <- c("c" = "f", "b" = "e", "a" = "f")
@@ -69,4 +78,34 @@ test_that("Renaming list with an conflicting variable name - silent", {
   replace_list <- c("c" = "f", "b" = "e", "a" = "f")
   expected_value <- list(f = 1, e = 2, f = 3)
   expect_identical(rename(x=x, replace=replace_list, duplicate_behavior=duplicate_behavior), expected=expected_value)
+})
+
+
+##
+## This batch tests the boundary cases
+##
+test_that("Renaming to the same value", {
+  x <- list(a = 1, b = 2, c = 3)
+  replace_list <- c("a" = "a")
+  expected_value <- x
+  expect_identical(rename(x=x, replace=replace_list), expected=expected_value)
+})
+test_that("Renaming list with an empty renaming vector", {
+  x <- list(a = 1, b = 2, c = 3)
+  replace_list <- c()
+  expected_value <- x
+  expect_identical(rename(x=x, replace=replace_list), expected=expected_value)
+})
+test_that("Swapping (shouldn't cause problems)", {
+  x <- list(a = 1, b = 2, c = 3)
+  replace_list <- c("c" = "f", "b" = "e", "a" = "c")
+  expected_value <- list(c = 1, e = 2, f = 3)
+  actual_value <- rename(x=x, replace=replace_list)
+  expect_identical(actual_value, expected=expected_value)
+})
+test_that("Multiple assignments for the same element", {
+  x <- list(a = 1, b = 2, c = 3)
+  replace_list <- c("a" = "d", "a" = "e", "a" = "f")
+  expected_value <- list(a = 1, a = 2, a = 3)
+  expect_message(rename(x=x, replace=replace_list))
 })
