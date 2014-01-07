0ec7a7357f26c21294d2b562852f3746bb415cea
Will Beasley <wibeasley@hotmail.com>
2014-01-06 22:41:03 -0600

    removed function qualifiers from `rename()`

--- R/rename.r
+++ R/rename.r
@@ -22,18 +22,18 @@
 rename <- function(x, replace, warn_missing = TRUE, duplicate_behavior = "warning" ) {
   
   # This line does the real work of `rename()`.
-  base::names(x) <- plyr::revalue(base::names(x), replace, warn_missing = warn_missing)
+  names(x) <- revalue(names(x), replace, warn_missing = warn_missing)
   
   # Check if any names are duplicated
-  tabled_values <- base::table(base::names(x))
-  duplicated_names <- base::names(tabled_values[tabled_values>1])
-  if( base::length(duplicated_names) > 0L ) {
-    #     response_message <- base::paste0("The plyr::rename operation has created duplicates for the following names: `", base::paste(duplicated_names, collapse="`, `"), "`")
-    response_message <- base::paste0("The plyr::rename operation has created duplicates for the following name(s): (`", base::paste(duplicated_names, collapse="`, `"), "`)")
-    if( duplicate_behavior == "error") base::stop(response_message)
-    else if( duplicate_behavior == "warning" ) base::warning(response_message)
-    else if( duplicate_behavior == "message" ) base::message(response_message)
-    else if( duplicate_behavior != "silent" ) base::stop("The plyr::rename value passed to the `duplicate_behavior` parameter was not recognized.")
+  tabled_values <- table(names(x))
+  duplicated_names <- names(tabled_values[tabled_values>1])
+  if( length(duplicated_names) > 0L ) {
+    #     response_message <- paste0("The plyr::rename operation has created duplicates for the following names: `", paste(duplicated_names, collapse="`, `"), "`")
+    response_message <- paste0("The plyr::rename operation has created duplicates for the following name(s): (`", paste(duplicated_names, collapse="`, `"), "`)")
+    if( duplicate_behavior == "error") stop(response_message)
+    else if( duplicate_behavior == "warning" ) warning(response_message)
+    else if( duplicate_behavior == "message" ) message(response_message)
+    else if( duplicate_behavior != "silent" ) stop("The plyr::rename value passed to the `duplicate_behavior` parameter was not recognized.")
   }  
   return( x )
 }
