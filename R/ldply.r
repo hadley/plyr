64a378f1890c7da23188239f58df3777dec4e017
dougmitarotonda <dougmitarotonda@gmail.com>
2015-03-31 12:20:41 -0700

    update .id = NULL in ldply
    Includes Hadley's comment on using `identical`

--- R/ldply.r
+++ R/ldply.r
@@ -18,7 +18,7 @@ ldply <- function(.data, .fun = NULL, ..., .progress = "none", .inform = FALSE,
     .progress = .progress, .inform = .inform,
     .parallel = .parallel, .paropts = .paropts)
 
-  if (is.na(.id)) {
+  if (identical(.id, NA) {
     .id <- ".id"
     id_as_factor <- FALSE
   } else
