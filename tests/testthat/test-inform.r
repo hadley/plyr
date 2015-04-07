15203fa2e7443c67d2e995a56866aa18985fff64
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-04-07 17:34:33 +0200

    silence error messages

--- tests/testthat/test-inform.r
+++ tests/testthat/test-inform.r
@@ -3,6 +3,9 @@ context("Informative messages")
 test_that(".inform parameter works for all l?ply functions", {
   funcnames <- ls(name = asNamespace("plyr"), pattern = "^l[^i]ply$")
 
+  old_options <- options(show.error.messages = FALSE)
+  on.exit(options(old_options), add = TRUE)
+
   input <- 1:10
   for (funcname in funcnames) {
     func <- get(funcname, asNamespace("plyr"))
@@ -12,6 +15,9 @@ test_that(".inform parameter works for all l?ply functions", {
 
 test_that(".inform parameter works for all d?ply functions", {
   funcnames <- ls(name = asNamespace("plyr"), pattern = "^d[^i]ply$")
+
+  old_options <- options(show.error.messages = FALSE)
+  on.exit(options(old_options), add = TRUE)
 
   input <- data.frame(a = 1:10)
   for (funcname in funcnames) {
@@ -23,6 +29,9 @@ test_that(".inform parameter works for all d?ply functions", {
 test_that(".inform parameter works for all a?ply functions", {
   funcnames <- ls(name = asNamespace("plyr"), pattern = "^a[^i]ply$")
 
+  old_options <- options(show.error.messages = FALSE)
+  on.exit(options(old_options), add = TRUE)
+
   input <- data.frame(a = 1:10)
   for (funcname in funcnames) {
     func <- get(funcname, asNamespace("plyr"))
@@ -32,6 +41,9 @@ test_that(".inform parameter works for all a?ply functions", {
 
 test_that(".inform parameter works for all m?ply functions", {
   funcnames <- ls(name = asNamespace("plyr"), pattern = "^m[^i]ply$")
+
+  old_options <- options(show.error.messages = FALSE)
+  on.exit(options(old_options), add = TRUE)
 
   input <- data.frame(a = 1:10)
   for (funcname in funcnames) {
