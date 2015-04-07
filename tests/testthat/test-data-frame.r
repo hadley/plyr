e3c122cbe27bbb803c006df8a8978497361dd838
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-04-07 17:34:56 +0200

    fix syntax error in tests

--- tests/testthat/test-data-frame.r
+++ tests/testthat/test-data-frame.r
@@ -66,15 +66,15 @@ test_that(".id column can be renamed or dropped", {
   l <- llply(1:4, function(i) rep(i, i))
   names(l) <- 1:4
   f <- function(l) data.frame(sum=sum(unlist(l)))
-  
+
   out1 <- ldply(l, f)
   out2 <- ldply(l, f, .id='x')
   out3 <- ldply(l, f, .id=NULL)
-  
+
   expect_equal(names(out1), c('.id', 'sum'))
   expect_equal(names(out2), c('x', 'sum'))
-  expect_equal(names(out3), c('sum')
-  
+  expect_equal(names(out3), c('sum'))
+
   expect_identical(out1$.id, names(l))
   expect_identical(out2$x, factor(names(l)))
 
