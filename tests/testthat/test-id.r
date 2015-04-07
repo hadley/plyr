5a374301d7d1223499b54f373336069c0cb76c2b
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-04-07 21:35:32 +0200

    add failing test for .id parameter in adply

--- tests/testthat/test-id.r
+++ tests/testthat/test-id.r
@@ -30,3 +30,47 @@ test_that(".id parameter for rdply", {
   expect_false(is.factor(rdply(10, 0, .id = "ID")$ID))
 
 })
+
+test_that(".id parameter for adply", {
+  data <- array(1:27, dim = c(3, 3, 3))
+
+  expect_equal(names(adply(data, 1, identity)), c("X1", "1", "2", "3"))
+  expect_equal(names(adply(data, 2, identity)), c("X1", "1", "2", "3"))
+  expect_equal(names(adply(data, 3, identity)), c("X1", "1", "2", "3"))
+  expect_equal(names(adply(data, c(1, 2), identity)), c("X1", "X2", "V1", "V2", "V3"))
+  expect_equal(names(adply(data, c(1, 3), identity)), c("X1", "X2", "V1", "V2", "V3"))
+  expect_equal(names(adply(data, c(1, 2, 3), identity)), c("X1", "X2", "X3", "V1"))
+  expect_equal(names(adply(data, c(), identity)), paste0("V", data))
+
+  expect_equal(names(adply(data, 1, identity, .id = NA)), c("X1", "1", "2", "3"))
+  expect_equal(names(adply(data, 2, identity, .id = NA)), c("X1", "1", "2", "3"))
+  expect_equal(names(adply(data, 3, identity, .id = NA)), c("X1", "1", "2", "3"))
+  expect_equal(names(adply(data, c(1, 2), identity, .id = NA)), c("X1", "X2", "V1", "V2", "V3"))
+  expect_equal(names(adply(data, c(1, 3), identity, .id = NA)), c("X1", "X2", "V1", "V2", "V3"))
+  expect_equal(names(adply(data, c(1, 2, 3), identity, .id = NA)), c("X1", "X2", "X3", "V1"))
+  expect_equal(names(adply(data, c(), identity, .id = NA)), paste0("V", data))
+
+  expect_equal(names(adply(data, 1, identity, .id = NULL)), c("1", "2", "3"))
+  expect_equal(names(adply(data, 2, identity, .id = NULL)), c("1", "2", "3"))
+  expect_equal(names(adply(data, 3, identity, .id = NULL)), c("1", "2", "3"))
+  expect_equal(names(adply(data, c(1, 2), identity, .id = NULL)), c("V1", "V2", "V3"))
+  expect_equal(names(adply(data, c(1, 3), identity, .id = NULL)), c("V1", "V2", "V3"))
+  expect_equal(names(adply(data, c(1, 2, 3), identity, .id = NULL)), c("V1"))
+  expect_equal(names(adply(data, c(), identity, .id = NULL)), paste0("V", data))
+
+  expect_equal(names(adply(data, 1, identity, .id = LETTERS[1])), c("A", "1", "2", "3"))
+  expect_equal(names(adply(data, 2, identity, .id = LETTERS[1])), c("A", "1", "2", "3"))
+  expect_equal(names(adply(data, 3, identity, .id = LETTERS[1])), c("A", "1", "2", "3"))
+  expect_equal(names(adply(data, c(1, 2), identity, .id = LETTERS[1:2])), c("A", "B", "V1", "V2", "V3"))
+  expect_equal(names(adply(data, c(1, 3), identity, .id = LETTERS[1:2])), c("A", "B", "V1", "V2", "V3"))
+  expect_equal(names(adply(data, c(1, 2, 3), identity, .id = LETTERS[1:3])), c("A", "B", "C", "V1"))
+
+  error_rx <- "[.]id argument.*number of margins"
+  expect_error(adply(data, 1, identity, .id = LETTERS[1:2]), error_rx)
+  expect_error(adply(data, 2, identity, .id = LETTERS[1:2]), error_rx)
+  expect_error(adply(data, 3, identity, .id = LETTERS[1:2]), error_rx)
+  expect_error(adply(data, c(1, 2), identity, .id = LETTERS[1:3]), error_rx)
+  expect_error(adply(data, c(1, 3), identity, .id = LETTERS[1:3]), error_rx)
+  expect_error(adply(data, c(1, 2, 3), identity, .id = LETTERS[1:4]), error_rx)
+  expect_error(adply(data, c(), identity, .id = LETTERS[1]), error_rx)
+})
