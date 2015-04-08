656e2effd95d323b836143f9b8888a2a1c06727d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-04-08 18:50:26 +0200

    remove usage to satisfy R CMD check

--- R/plyr-deprecated.r
+++ R/plyr-deprecated.r
@@ -3,9 +3,11 @@
 #' These functions are provided for compatibility with older versions of
 #' \code{plyr} only, and may be defunct as soon as the next release.
 #'
-#' @usage
-#' liply(...)
-#' isplit2(...)
+#' \itemize{
+#'   \item \code{\link{liply}}
+#'   \item \code{\link{isplit2}}
+#' }
+#'
 #' @name plyr-deprecated
 # EXCLUDE COVERAGE START
 NULL
