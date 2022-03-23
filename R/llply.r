84157e91cacb9bd5ad039960170306902cdb2cfb
Hadley Wickham <h.wickham@gmail.com>
2022-03-23 13:39:24 -0500

    Use non-vectorised comparison
    Closes #304

--- R/llply.r
+++ R/llply.r
@@ -27,7 +27,7 @@ llply <- function(.data, .fun = NULL, ..., .progress = "none", .inform = FALSE,
     pieces <- as.list(.data)
 
     # This special case can be done much faster with lapply, so do it.
-    fast_path <- .progress == "none" && !.inform && !.parallel
+    fast_path <- identical(.progress, "none") && !.inform && !.parallel
     if (fast_path) {
       return(structure(lapply(pieces, .fun, ...), dim = dim(pieces)))
     }
